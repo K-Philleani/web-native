8e155f6e0903cf48b1da2b2ad79493d83be21fb0
K-Philleani <964202623@qq.com>
2020-11-09 19:58:35 +0800

    Hello World

--- main.go
+++ main.go
@@ -1,1 +1,16 @@
-package web_native
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+// 处理器
+func handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello World", r.URL.Path)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8087", nil)
+}
