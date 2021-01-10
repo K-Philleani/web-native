dc34902271f045aea4dd49da223f16b57101ca86
K-Philleani <964202623@qq.com>
2021-01-10 23:36:18 +0800

    main

--- main.go
+++ main.go
@@ -1,5 +1,15 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"net/http"
+)
 
+func Handler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "this is HandleFunc", r.URL.Path)
+}
+
+func main() {
+	http.HandleFunc("/start", Handler)
+	http.ListenAndServe(":9090", nil)
 }
